3c583c2fa575444c109a55ccd17c0449e75d097b
Zolli0217 <zoltanbodi61@gmail.com>
2026-04-16 13:59:10 +0200

    Add project files.

---
+++ Jatekos.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace WpfAppLetra.Models
+{
+    public class Jatekos
+    {
+        string nev;
+        SolidColorBrush szin;
+        List<int> lepesek;
+
+        public Jatekos(string nev, SolidColorBrush szin)
+        {
+            this.nev = nev;
+            this.szin = szin;
+            lepesek = new List<int>();
+        }
+        public Jatekos(string nev, SolidColorBrush szin, List<int> eddigiLepesek)
+        {
+            this.nev = nev;
+            this.szin = szin;
+            lepesek = eddigiLepesek;
+        }
+
+        public string Nev { get => nev; }
+        public SolidColorBrush Szin { get => szin; }
+        public List<int> Lepesek { get => lepesek; }
+
+        public int Pozicio { get => lepesek[lepesek.Count - 1]; }
+
+        public void Lep(int ujmezo)
+        {
+            lepesek.Add(ujmezo);
+        }
+
+        public void Ujrakzedes()
+        {
+            lepesek.Clear();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfAppLetra.Models;
+
+namespace letraWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SolidColorBrush szin;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Sli_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            byte red = (byte)redSli.Value;
+            byte green = (byte)greenSli.Value;
+            byte blue = (byte)blueSli.Value;
+
+            szin = new SolidColorBrush(Color.FromRgb((byte)red, (byte)green, (byte)blue));
+
+            colorEll.Fill = szin;
+        }
+
+        List<Jatekos> jatekosok = new List<Jatekos>();
+        private void regBtn(object sender, RoutedEventArgs e)
+        {
+            Jatekos nev = new Jatekos(playerNameTb.Text, szin as SolidColorBrush);
+
+            if (playerNameTb.Text == "")
+            {
+                MessageBox.Show("Adj meg egy nevet!");
+                return;
+            }
+
+            var item = new ListBoxItem()
+            {
+                Content = nev.Nev,
+                Foreground = nev.Szin
+            };
+
+            jatekosok.Add(nev);
+
+            jatekosLb.Items.Add(item);
+
+            jatekosNeve.Content = $"jatékos: {nev.Nev}";
+        }
+
+        int allas = 0;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {   if(jatekosok.Count == 0)
+            {
+                MessageBox.Show("Adj hozzá játékost!");
+                return;
+            }
+
+            Random rnd = new Random();
+            int dobas = rnd.Next(1, 7);
+            allas += dobas;
+
+            eredmenyLb.Content = dobas;
+            Output.Items.Add(allas);
+
+            if(allas % 10 == 0)
+            {
+                MessageBox.Show("Megcsúsztál, lépj vissza 3 mezőt!");
+                allas -= 3;
+                Output.Items.Add(allas);
+            }
+
+            if(allas >= 45)
+            {
+                MessageBox.Show("Nyertél!");
+                allas = 0;
+                Output.Items.Clear();
+                eredmenyLb.Content = "";
+            }
+        }
+    }
+}
